5f2fca0239c772f562a636299fe1815fc11c8253
michellebessa <michelleandressa12@gmail.com>
2022-10-30 17:22:54 +0000

    my first commit

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,61 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Media(Base):
+    __tablename__ = 'Media'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_login = Column(String(250), nullable=False)
+    user_password = Column(String(250), nullable=False)
+    user = Column(ForeignKey('User.id'))
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    DOB = Column(Integer, nullable=True)
+    phone_number = Column(Integer, nullable=True)
+    gender = Column(String(250), nullable=False)
+    follower = Column(ForeignKey('Follower.id'))
+    post = Column(ForeignKey('Post.id'))
+
+
+class Follower(Base):
+    __tablename__ = 'Follower'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    DOB = Column(Integer, nullable=True)
+    phone_number = Column(Integer, nullable=True)
+    gender = Column(String(250), nullable=False) 
+
+
+class Post(Base):
+    __tablename__ = 'Post'
+    id = Column(Integer, primary_key=True) ## ID of the user who owns this post
+    caption = Column(String(250), nullable=True) ## Photo caption
+    user_id = Column(Integer, nullable=True) ##ID of the user who owns this photo
+    like_numbers = Column(Integer, nullable=True) ## how many likes the photo has
+    latitude = Column(Float, nullable=True) ## Latitude value for location
+    longitude = Column(Float, nullable=True) ## Longitude value for location
+    image_path = Column(String(250), nullable=True) ## Path to image on server
+    image_size = Column(Integer, nullable=True) ## Image size on server
+    date_created = Column(DateTime, nullable=True) ## When image was created 
+    date_updated = Column(DateTime, nullable=True) ## Last time image was updated
+    comment = Column(ForeignKey('Comment.id'))
+
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(Integer, primary_key=True)
+    date_created = Column(DateTime, nullable=True)
+    date_updated = Column(DateTime, nullable=True) 
+    comment = Column(String(250), nullable=True)
 
     def to_dict(self):
         return {}
